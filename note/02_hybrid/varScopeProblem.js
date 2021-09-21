a5b489a22bd2fdf404c57ff700d445e89a7177ba
andrea <kjkandrea@gmail.com>
2021-09-21 18:35:17 +0900

    Add var scope problem;

--- note/02_hybrid/varScopeProblem.js
+++ note/02_hybrid/varScopeProblem.js
@@ -4,8 +4,20 @@ for (var i = 0; i < 3; i++) {
   }, 100)
 }
 
-for (let i = 0; i < 3; i++) {
+try {
+  console.log(i)
+} catch (err) {
+  console.error('i is undefined')
+} // 3 🙃
+
+for (let j = 0; j < 3; j++) {
   setTimeout(() => {
-    console.log(`i : ${i}`) // i: 0, i: 1, i: 2
+    console.log(`j : ${j}`) // j: 0, j: 1, j: 2
   }, 100)
 }
+
+try {
+  console.log(j)
+} catch (err) {
+  console.error('j is undefined')
+} // err
