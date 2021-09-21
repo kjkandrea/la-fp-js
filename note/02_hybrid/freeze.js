a2224652945f6a88826d63b4cfce224c99515a65
andrea <kjkandrea@gmail.com>
2021-09-21 17:35:34 +0900

    Use lense

--- note/02_hybrid/freeze.js
+++ note/02_hybrid/freeze.js
@@ -1,4 +1,11 @@
 import Person from '../../model/person.js'
 
+import R from 'ramda'
+
 const person = new Person(false, 'KIM', 'JK', '1992-01-07')
-console.log(person)
+const lastnameLens = R.lensProp('lastname')
+const newPerson = R.set(lastnameLens, 'SJ', person)
+console.log(
+  R.view(lastnameLens, person),
+  R.view(lastnameLens, newPerson)
+)
