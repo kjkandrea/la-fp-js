ea54d5dd5838b263dce28a77cffe7b6d2f3ef330
andrea <kjkandrea@gmail.com>
2021-09-22 14:53:33 +0900

    Add bench test
    recursive 가 쳐발리는데..

--- note/03_enhancing/recursion.js
+++ note/03_enhancing/recursion.js
@@ -1,11 +1,47 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
 function sum (arr) {
-  return _.isEmpty(arr)
+  return arr.length > 1
     ? 0
     : arr[0] + sum(arr.splice(1, arr.length))
 }
 
-console.log(
-  sum([1, 2, 3, 4, 5, 6, 7, 8, 9]),
-)
+function forSum (arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++ ) {
+    result += arr[i]
+  }
+  return result;
+}
+
+(() => {
+  console.group('recursion')
+  console.time()
+  console.log(
+    sum([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+  )
+  console.timeEnd()
+  console.groupEnd()
+})();
+
+(() => {
+  console.group('reduce')
+  console.time()
+  console.log(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((a, b) => a + b)
+  )
+  console.timeEnd()
+  console.groupEnd()
+})();
+
+(() => {
+  console.group('for')
+  console.time()
+  console.log(
+    forSum([1, 2, 3, 4, 5, 6, 7, 8, 9])
+  )
+  console.timeEnd()
+  console.groupEnd()
+})();
+
+console.log('result : reduce = for > recursion')
