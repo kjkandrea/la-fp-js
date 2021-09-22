77cdaf9693e1e42578fe4aa16ed372b67042251c
andrea <kjkandrea@gmail.com>
2021-09-22 17:14:51 +0900

    Add other logic

--- dojo/compose_with_other_module.js
+++ dojo/compose_with_other_module.js
@@ -4,12 +4,15 @@ const students = ['MM', 'SJ', 'JK', 'YS']
 const grades = [10, 100, 90, 80]
 
 const smartestStudent = R.compose(
-  R.sortBy(R.prop(1)),
+  R.head,
+  R.pluck(0),
+  R.reverse,
+  R.sortBy(R.prop(1)), // 튜플 오른쪽 인수(grade) 기준으로 정렬.
   R.zip
 )
 
 function main() {
   console.log(
-    smartestStudent(students, grades)
+    smartestStudent(students, grades) // SJ
   )
 }main()
