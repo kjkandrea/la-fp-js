f3242fd6cb34eec5f0f17b2663f74b2a09bdc0d6
andrea <kjkandrea@gmail.com>
2021-09-22 17:12:17 +0900

    Create ramda compose tutorial

---
+++ dojo/compose_with_other_module.js
@@ -0,0 +1,14 @@
+import R from 'ramda'
+
+const students = ['MM', 'SJ', 'JK', 'YS']
+const grades = [10, 100, 90, 80]
+
+const smartestStudent = R.compose(
+  R.zip
+)
+
+function main() {
+  console.log(
+    smartestStudent(students, grades)
+  )
+}main()
